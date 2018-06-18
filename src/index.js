a788d4983207b4baee67ffea5af3d48290775299
Taha Iftekhar <taha_iftekhar@yahoo.com>
2018-06-18 20:19:35 +0500

    Initial Commit with router

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import App from './components/app';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+        <div>
+            <Route path="/" component={App}/>
+        </div>
+    </BrowserRouter>
+  </Provider>
+  , document.querySelector('.container'));
