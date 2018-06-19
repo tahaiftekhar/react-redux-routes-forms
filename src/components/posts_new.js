66b81ffb299c20dbc446cad6f154e53eb276337c
Taha Iftekhar <taha_iftekhar@yahoo.com>
2018-06-19 17:42:37 +0500

    Made new component and introduced SWITCH for routing

---
+++ src/components/posts_new.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class PostsNew extends Component{
+    render(){
+        return (
+            <div>
+                Post New
+            </div>
+        );
+    }
+}
+
+export default PostsNew;
