9acb8fa753bda7d06d89f9f10c1adc8b370c93cb
Taha Iftekhar <taha_iftekhar@yahoo.com>
2018-06-19 17:07:21 +0500

    Rendered data (posts) on screen

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import _ from 'lodash';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions/index';
 
@@ -7,11 +8,27 @@ class PostsIndex extends Component{
         this.props.fetchPosts();
     }
 
+    renderPosts(){
+        //Because default map function only works on Array. This is an object
+        return _.map(this.props.posts, post=>{
+            return(
+                <li className="list-group-item" key={post.id}>{post.title}</li>
+            );
+        });
+    }
+
     render(){
         return (
-            <div>Posts Index</div>
+            <div>
+                <h3>Posts</h3>
+                <ul className="list-group">{this.renderPosts()}</ul>
+            </div>
         );
     }
 }
 
-export default connect(null, { fetchPosts })(PostsIndex);
+function mapStateToProps(state){
+    return {posts: state.posts};
+}
+
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
