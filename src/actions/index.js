29634dae054d8e4da989191d12df442a33ab69de
Taha Iftekhar <taha_iftekhar@yahoo.com>
2018-06-21 11:01:42 +0500

    Made action creator to fetch a single post and made a reducer for that....

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'fetch_posts';
+export const FETCH_POST = 'fetch_post';
 export const CREATE_POST = 'create_post';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
@@ -23,6 +24,16 @@ export function createPost(values, callback){
 
     return{
         type: CREATE_POST,
+        payload: request
+    };
+}
+
+export function fetchPost(id){
+    const url = `${ROOT_URL}/posts/${id}${API_KEY}`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_POST,
         payload: request
     };
 }
--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,11 +1,16 @@
 import _ from 'lodash';
 import { FETCH_POSTS } from '../actions/index';
+import { FETCH_POST } from '../actions/index';
 
 export default function(state = {}, action){
     switch (action.type){
+        case FETCH_POST:
+            const post = action.payload.data;
+            return {...state, [post.id]: post};
         case FETCH_POSTS:
             const posts = _.mapKeys(action.payload.data, 'id');
             return posts;
     }
     return state;
 };
+
