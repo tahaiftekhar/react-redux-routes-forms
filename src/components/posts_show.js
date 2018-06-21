ea21d081c1322ff1561431e85a0a720c6b6ec050
Taha Iftekhar <taha_iftekhar@yahoo.com>
2018-06-21 12:15:15 +0500

    Calling fetchPost action creator

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions/index';
 
 class PostsShow extends Component{
+    componentDidMount(){
+        const { id } = this.props.match.params;
+        // For const { id, a, b, c } = this.props.match.params; if multiple route args
+        //or const id = this.props.match.params.id;
+        this.props.fetchPost(id);
+    }
     render(){
         return(
             <div>Posts Show</div>
@@ -8,4 +16,4 @@ class PostsShow extends Component{
     }
 }
 
-export default PostsShow;
+export default connect(null, { fetchPost })(PostsShow);
